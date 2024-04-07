500f5f0bf7863102cd04e8582dffa0659b29bf4b
danihek07 <danihek07@gmail.com>
2024-04-07 20:47:58 +0000

    fixed error with installation

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ cp -r data memoryshell.sh $msPath
 echo -e "\n(Change questions and answers path for your own dataset, 0 at the end mean that you have to answer, any other character or number will just show the answer)"
 echo -e "\nAdd this line to the end of your ~/.zshrc or other shell:\n"
 echo -e "---------| MemoryShell / メモリーシェル |---------\n"
-echo "$msPath/memoryshell.sh ./data/hiragana_questions.txt ./data/hiragana_answers 0"
+echo "$msPath/memoryshell.sh $msPath/data/hiragana_questions.txt $msPath/data/hiragana_answers.txt 0"
 echo -e "\n--------------------------------------------------\n"
